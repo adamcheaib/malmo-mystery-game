7e107b97eebb8b8c30d3b1d5d1a21789a1e85784
nelsaj <112635402+nelsaj@users.noreply.github.com>
2024-04-22 17:10:56 +0200

    Tracking buttons

--- index.js
+++ index.js
@@ -1,43 +1,121 @@
 // var map = L.map('map').setView([55.60275864327367, 13.000073510709273], 13);
 const statue_coords = 
     {
-        storTorg: {latitude: 55.606749499890064, longitude: 13.000073510709273, color: "red"},
-        folketsPark: {latitude: 55.59347581530123, longitude: 13.013949793519787, color: "green"},
-        soderTull: {latitude: 55.60132802122993, longitude: 13.000414613334193, color: "blue"},
-        mollan: {latitude: 55.591883687522206, longitude: 13.007768948210334, color: "yellow"},
-        vallGatan: {latitude: 55.607391899774534, longitude: 12.99839459721525, color: "orange"},
-        kungsParken: {latitude: 55.60371767788408, longitude: 12.992158258580288, color: "purple"},
+        adolf: {latitude: 55.606749499890064, longitude: 13.000073510709273, color: "red"},
+        gass: {latitude: 55.602315039588795, longitude: 12.98734215319501, color: "green"},
+        katt: {latitude: 55.60132802122993, longitude: 13.000414613334193, color: "blue"},
+        tungsinnet: {latitude: 55.603156508261634, longitude: 13.00720665598558, color: "yellow"},
+        frans: {latitude: 55.607391899774534, longitude: 12.99839459721525, color: "orange"},
+        radjur: {latitude: 55.60371767788408, longitude: 12.992158258580288, color: "purple"},
     }
+const main = [55.604096980734305, 12.996309487293441];
 
-navigator.geolocation.getCurrentPosition((position) => {
-    // create map
-    var map = L.map('map', {enableHighAccuracy: true, watch: true}).setView([position.coords.latitude, position.coords.longitude], 15);
+navigator.geolocation.getCurrentPosition(createMap, function (er) {console.log(er)}, {enableHighAccuracy: true});
+function createMap (position) {
 
+    // map
+    var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 15);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        maxZoom: 20,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    
     }).addTo(map);
 
     // create marker
     var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
 
-
     // create circles
+    L.circle(main, {
+        color: "black",
+        stroke: false,
+        // fillColor: statue_coords[location]["color"],
+        fillOpacity: 0.25,
+        radius: 800
+    }).addTo(map);
+
     for (const location in statue_coords) {
-        L.circle([statue_coords[location]["latitude"], statue_coords[location]["longitude"]], {
+        let circle = L.circle([statue_coords[location]["latitude"], statue_coords[location]["longitude"]], {
             color: statue_coords[location]["color"],
             fillColor: statue_coords[location]["color"],
             fillOpacity: 0.5,
-            radius: 100
+            radius: 50
         }).addTo(map);
     }
 
-    setInterval( () => {
-        navigator.geolocation.getCurrentPosition((position) => marker.setLatLng([position.coords.latitude, position.coords.longitude]));
-        
-        // map.on('locationfound', function(ev){
-        //     console.log(ev.latlng);
-        //     marker.setLatLng(ev.latlng);
-        // })
-    }, 1000 );
-})
+    map.locate({watch: true, enableHighAccuracy: true, timeout: 2000, maximumAge: Infinity})
+        .on("locationfound", e => {console.log(e.latitude, e.longitude); marker.setLatLng([e.latitude, e.longitude]);})
+        .on("locationerror", e => {console.log(e)})
+
+    document.getElementById("test").addEventListener("click", e => {
+        map.flyTo([position.coords.latitude, position.coords.longitude], 18, {duration: 0.8});
+    });
+    document.getElementById("test2").addEventListener("click", e => {
+        map.flyTo(main, 14, {duration: 0.8});
+    });
+}
+
+
+// navigator.geolocation.watchPosition(onLocationFound, onLocationError, {
+//     enableHighAccuracy: true,
+//     maximumAge: 1000,
+//     timeout: 1000
+//   });
+//   function onLocationFound (e) {
+//     marker.setLatLng([e.coords.latitude, e.coords.longitude]);
+//     console.log(e);
+//   }
+//   function onLocationError (e) {
+//     console.log("error", e);
+//   }
+
+
+
+
+
+// navigator.geolocation.getCurrentPosition((position) => {
+//     // create map
+//     // {enableHighAccuracy: true, watch: true, timeout: 2000}
+//     var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 15);
+//     console.log(map.locate());
+
+//     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         maxZoom: 20,
+//         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//     }).addTo(map);
+ 
+//     // create marker
+//     var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+    
+//     map.locate({watch: true, enableHighAccuracy: true, timeout: 2000, maximumAge: Infinity})
+//         .on("locationfound", e => {console.log(e.latitude, e.longitude); marker.setLatLng([e.latitude, e.longitude]);})
+//         .on("locationerror", e => {console.log(e)})
+
+//     // create circles
+//     for (const location in statue_coords) {
+//         let circle = L.circle([statue_coords[location]["latitude"], statue_coords[location]["longitude"]], {
+//             color: statue_coords[location]["color"],
+//             fillColor: statue_coords[location]["color"],
+//             fillOpacity: 0.5,
+//             radius: 50
+//         }).addTo(map);
+//         circle.on("click", e => {console.log(location)});
+//     }
+
+
+//     // navigator.geolocation.watchPosition(onLocationFound, onLocationError, {
+//     //     enableHighAccuracy: true,
+//     //     maximumAge: 1000,
+//     //     timeout: 1000
+//     //   });
+//     //   function onLocationFound (e) {
+//     //     marker.setLatLng([e.coords.latitude, e.coords.longitude]);
+//     //     console.log(e);
+//     //   }
+//     //   function onLocationError (e) {
+//     //     console.log("error", e);
+//     //   }
+
+
+//     document.getElementById("test").addEventListener("click", e => {
+//         map.flyTo([position.coords.latitude, position.coords.longitude], 18, {duration: 0.8});
+//     });
+// })
