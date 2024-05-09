8e8711b303f8d0c090a2e809f29a20f3198a4d09
nelsaj <112635402+nelsaj@users.noreply.github.com>
2024-05-09 14:32:54 +0200

    New games

--- games/rock_paper/game.js
+++ games/rock_paper/game.js
@@ -1,72 +1,65 @@
 // fun variables wooho
-const options = ["rock", "paper", "scissors"];
-const chart =
+const options = ["sten", "sax", "påse"];
+const chart = 
+{
+    sten: 
     {
-        rock:
-            {
-                weak: "paper",
-                strong: "scissors"
-            },
-        scissors:
-            {
-                weak: "rock",
-                strong: "paper"
-            },
-        paper:
-            {
-                weak: "scissors",
-                strong: "rock"
-            },
-    }
+        weak: "påse",
+        strong: "sax"
+    },
+    sax: 
+    {
+        weak: "sten",
+        strong: "påse"
+    },
+    påse: 
+    {
+        weak: "sax",
+        strong: "sten"
+    },
+}
 let scoreCounter = 0;
 let scoreCounter_op = 0;
 
 
 // some functions woooohoo
-function randomOption() {
-    return options[Math.round(Math.random() * 2)];
-}
+function randomOption () {return options[Math.round(Math.random() * 2)];}
 
-function winOrLose(cpu, player) {
-    if (chart[player].weak == cpu) {
-        scoreCounter_op++;
-        return "LOSE"
-    }
-    ;
-    if (chart[player].strong == cpu) {
-        scoreCounter++;
-        return "WIN"
-    }
-    ;
-    if (player == cpu) return "TIE";
+function winOrLose (cpu, player) {
+    if(chart[player].weak == cpu) {scoreCounter_op++; return "FÖRLORAR"};
+    if(chart[player].strong == cpu) {scoreCounter++; return "VINNER"};
+    if(player == cpu) return "OAVGJORT";
 }
-
-function gameOver(result) {
-    document.getElementById("result").textContent = `YOU ${result} THE GAME`;
+function gameOver (result, timer = 10) {
+    document.getElementById("result").textContent = `DU ${result} SPELET`;
     document.querySelectorAll("#options > button").forEach(button => button.disabled = true);
-    localStorage.setItem("close_iframe", true);
+    
+    if(result == "VANN") localStorage.setItem("close_iframe", true);
 
-    if (result == "LOST") {
+    if(result == "FÖRLORAR") 
+    {
         const tryagain_btn = document.getElementById("tryagain");
         tryagain_btn.classList.remove("hidden");
+        document.getElementById("cpu_player_move").textContent = "Lycka till nästa gång!";
 
         // timer time
-        let timer = 9;
-        let interval = setInterval(() => {
-                // if time is out
-                if (timer == 0) {
-                    clearInterval(interval);
-                    tryagain_btn.disabled = false;
-                    tryagain_btn.querySelector("span").textContent = "";
-                    return
-                }
+        tryagain_btn.querySelector("span").textContent = `om ${timer}s`; tryagain_btn.disabled = true;
+        let interval = setInterval(() => 
+        {
+            // if time is out
+            timer--;
+            if(timer == 0) {clearInterval(interval); localStorage.removeItem("timer"); tryagain_btn.disabled = false; tryagain_btn.querySelector("span").textContent = ""; return};
+            tryagain_btn.querySelector("span").textContent = `om ${timer}s`;
 
-                tryagain_btn.querySelector("span").textContent = `in ${timer}s`;
-                timer--;
-            },
-            1000);
-    }
-    ;
+            localStorage.setItem("timer", timer);
+        }, 
+        1000);
+    };
+}
+// no reload cheating
+if(localStorage.getItem("timer")) {
+    gameOver("FÖRLORAR", Number(localStorage.getItem("timer")));
+    document.getElementById("tryagain").disabled = true;
 }
 
 // the game(tm)
@@ -74,36 +67,25 @@ document.querySelectorAll("#options > button").forEach(button => {
 
     button.addEventListener("click", e => {
         // check if game is over
-        if (scoreCounter == 3) {
-            gameOver("WON");
-            return
-        }
-        ;
-        if (scoreCounter_op == 3) {
-            gameOver("LOST");
-            return
-        }
-        ;
+        if(scoreCounter == 3) {gameOver("VANN"); return};
+        if(scoreCounter_op == 3) {gameOver("FÖRLORAR"); return};
 
         // retriving the moves
         const player_move = e.target.textContent;
         const cpu_player_move = randomOption();
 
         // rendering the result
-        document.getElementById("cpu_player_move").textContent = "Opponent goes: " + randomOption();
-        document.getElementById("result").textContent = "YOU " + winOrLose(player_move.toLowerCase(), cpu_player_move.toLowerCase());
+        const result = winOrLose(cpu_player_move.toLowerCase(), player_move.toLowerCase());
+        document.getElementById("cpu_player_move").textContent = "Motståndaren valde: " + cpu_player_move;
+        document.getElementById("result").textContent = result == "OAVGJORT" ? result : `DU ${result}`;
     })
 
 })
 
 document.getElementById("tryagain").addEventListener("click", e => {
     // resets everything
-    scoreCounter = 0;
-    scoreCounter_op = 0;
-    document.querySelectorAll("#options > button").forEach(button => button.disabled = false);
-    document.getElementById("tryagain").disabled = true;
-    document.getElementById("cpu_player_move").textContent = "";
-    document.getElementById("result").textContent = "";
-    document.querySelector("#tryagain span").textContent = "in 10s";
+    scoreCounter = 0; scoreCounter_op = 0;
+    document.querySelectorAll("#options > button").forEach(button => button.disabled = false); document.getElementById("tryagain").disabled = true;
+    document.getElementById("cpu_player_move").textContent = ""; document.getElementById("result").textContent = ""; document.querySelector("#tryagain span").textContent = "in 10s";
     e.target.classList.add("hidden");
 })
